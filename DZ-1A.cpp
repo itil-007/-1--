dca96bf5be4f1dfcb6685475149eb8780a5951e0
Sergei Arutynyan <sergei995007@mail.ru>
2023-03-15 00:56:36 +0300

    DZ-1a

---
+++ DZ-1A.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+void foo(int arr[12])
+{
+    for (int value = 1, index = 0; index < 12; value += 2)
+        if (value % 5)
+        {
+            arr[index] = value;
+            index++;
+        }
+
+    for (int i = 0; i < 12; i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+
+    std::cout << std::endl;
+}
+
+int main()
+{
+    int arr[12]{};
+    foo(arr);
+
+    return 0;
+}
